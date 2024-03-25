d2155cf3b9b62cb06cbb7fd65339ccfd0a4bec48
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-03-25 16:26:57 -0300

    .

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -6,9 +6,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
-
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
